0d375eaab6b2b5cc62aa67de4d98c1ede933f07d
Alex Kiab <alexkiab-org@hotmail.com>
2024-05-16 12:30:15 +0200

    feat: Add button wich display when the user is logged

--- src/app/share/footer/footer.component.ts
+++ src/app/share/footer/footer.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { LogInService } from '../log-in.service';
+import { MatDialog } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
@@ -10,4 +13,22 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  loggedUserName: string | null = null;
+  private subscription!: Subscription;
+
+  constructor(
+    public loginPanelService: LogInService,
+    public dialog: MatDialog
+  ) {}
+
+  ngOnInit(): void {
+    this.subscription = this.loginPanelService.userName$.subscribe(
+      (logInUsuarioService) => (this.loggedUserName = logInUsuarioService)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
