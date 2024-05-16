6cffa60f3cf46d1ab2c64d1b0e0ed6f66e34e975
Alex Kiab <alexkiab-org@hotmail.com>
2024-05-16 11:39:30 +0200

    feat: Add dialog to show errors

--- src/app/pages/login/dialog-error/dialog-error.component.spec.ts
+++ src/app/pages/login/dialog-error/dialog-error.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { DialogErrorComponent } from './dialog-error.component';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+describe('DialogErrorComponent', () => {
+  let component: DialogErrorComponent;
+  let fixture: ComponentFixture<DialogErrorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [DialogErrorComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(DialogErrorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
