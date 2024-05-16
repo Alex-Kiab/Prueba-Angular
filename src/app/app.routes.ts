896bade0325e72b24e63e44e0cedb1afbe095e76
Alex Kiab <alexkiab-org@hotmail.com>
2024-05-16 11:38:35 +0200

    feat: Add table page with its route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './pages/login/login-page/login.component';
+import { TableComponent } from './pages/table/table.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'tabla',
+    component: TableComponent,
   },
 ];
